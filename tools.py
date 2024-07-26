d1c38a1ef4b35610eadb5a10204a36c77d8b197f
ToxicHahn <adrian.grieger@proton.me>
2024-07-26 03:01:53 +0200

    -

--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
 import re
  
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,10}\b'
  
 def checkEmailFormat(email):
     return re.fullmatch(regex, email)
